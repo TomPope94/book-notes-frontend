4832d21c1a9900c7d7495375a2fc196be1e51b6b
Tom Pope <Tpope@libertylondon.com>
2019-12-02 17:35:56 +0000

    adds some basic formatting for books in library view

--- src/components/elements/Book.js
+++ src/components/elements/Book.js
@@ -3,15 +3,22 @@ import { Link, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { BOOKS_ADD, BOOKS_DETAILS } from "constants/routes";
 import { getBook } from "actions/books";
+import anime from "animejs";
 
 const Book = ({ id, bookTitle, addBook, getBook }) => {
   const styles = {
     book: {
-      boxShadow: "0 1px 3px #222641",
+      boxShadow: "0 2px 4px #222641",
       cursor: "pointer",
       height: 300,
       width: 200,
-      margin: 50
+      margin: 50,
+      borderRadius: 5,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 10,
+      textAlign: "center"
     },
     addBook: {
       border: "1px solid #f38b66",
@@ -23,6 +30,18 @@ const Book = ({ id, bookTitle, addBook, getBook }) => {
   };
 
   const history = useHistory();
+
+  const enlargeBook = (hover, e) => {
+    const animateDirection = hover ? "normal" : "reverse";
+
+    anime({
+      targets: e.target,
+      scale: [1, 1.25],
+      duration: 500,
+      easing: "cubicBezier(0.675, 0.000, 0.330, 1.000)",
+      direction: animateDirection
+    });
+  };
 
   const openBook = () => {
     getBook(id);
@@ -39,8 +58,11 @@ const Book = ({ id, bookTitle, addBook, getBook }) => {
     <div
       style={{ ...styles.book, ...styles.existingBook }}
       onClick={() => openBook()}
+      className="book"
+      onMouseOver={e => enlargeBook(true, e)}
+      onMouseOut={e => enlargeBook(false, e)}
     >
-      <p>{bookTitle}</p>
+      {bookTitle}
     </div>
     // </Link>
   );
