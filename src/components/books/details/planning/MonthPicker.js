6ac9a977a70d0422877e3de6ee3ec6fe05f999f5
tompope94 <tompope1994@gmail.com>
2020-01-15 13:27:22 +0000

    changes arrows on month picker

--- src/components/books/details/planning/MonthPicker.js
+++ src/components/books/details/planning/MonthPicker.js
@@ -1,17 +1,36 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
 
-import { DayPickedContext } from "components/books/details/planning/dayPicked-context";
+import { DayPickedContext } from 'components/books/details/planning/dayPicked-context';
 
 const styles = {
   contentContainer: {
-    display: "flex",
-    justifyContent: "center",
-    width: "60%",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '60%',
     marginTop: 50
   },
   dateContainer: {
-    fontSize: "1.5rem"
+    fontSize: '1.5rem'
+  },
+  arrowLeft: {
+    width: 0,
+    height: 0,
+    borderTop: '15px solid transparent',
+    borderBottom: '15px solid transparent',
+    borderRight: '15px solid rgba(243,139,102, 0.5)',
+    cursor: 'pointer',
+    marginRight: 20
+  },
+  arrowRight: {
+    width: 0,
+    height: 0,
+    borderTop: '15px solid transparent',
+    borderBottom: '15px solid transparent',
+    borderLeft: '15px solid rgba(243,139,102, 0.5)',
+    cursor: 'pointer',
+    marginLeft: 20
   }
 };
 
@@ -22,32 +41,30 @@ const MonthPicker = () => {
         return (
           <div style={styles.contentContainer}>
             <div
+              style={styles.arrowLeft}
               onClick={() =>
                 changeState({
                   ...state,
                   monthChosen: moment(state.monthChosen)
-                    .subtract(1, "months")
-                    .format("MMM-YYYY")
+                    .subtract(1, 'months')
+                    .format('MMM-YYYY')
                 })
               }
-            >
-              Left
-            </div>
+            ></div>
             <div style={styles.dateContainer}>
               <p>{state.monthChosen}</p>
             </div>
             <div
+              style={styles.arrowRight}
               onClick={() =>
                 changeState({
                   ...state,
                   monthChosen: moment(state.monthChosen)
-                    .add(1, "months")
-                    .format("MMM-YYYY")
+                    .add(1, 'months')
+                    .format('MMM-YYYY')
                 })
               }
-            >
-              Right
-            </div>
+            ></div>
           </div>
         );
       }}
