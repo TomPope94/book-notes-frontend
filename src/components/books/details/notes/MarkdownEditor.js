c5eda8fb280eb1db99af5027d9541fb7d26ec2b0
tompope94 <tompope1994@gmail.com>
2020-02-24 07:29:42 +0000

    adds format buttons and hotkeys - ready for adding in full functionality

--- src/components/books/details/notes/MarkdownEditor.js
+++ src/components/books/details/notes/MarkdownEditor.js
@@ -5,14 +5,122 @@ import { Slate, Editable, withReact } from 'slate-react';
 import Element from 'components/books/details/notes/Element';
 import Leaf from 'components/books/details/notes/Leaf';
 
+const LIST_TYPES = ['numbered-list', 'bulleted-list'];
+
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold,
+      universal: true
+    });
+
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code'
+    });
+
+    return !!match;
+  },
+
+  togggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      {
+        bold: isActive ? null : true
+      },
+      {
+        match: n => Text.isText(n),
+        split: true
+      }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      {
+        type: isActive ? null : 'code'
+      },
+      {
+        match: n => Editor.isBlock(editor, n)
+      }
+    );
+  }
+};
+
 const MarkdownEditor = ({ value, changevalue }) => {
   const editor = useMemo(() => withReact(createEditor()), []);
 
   const renderElement = useCallback(props => <Element {...props} />, []);
   const renderLeaf = useCallback(props => <Leaf {...props} />, []);
 
+  const isMarkActive = (editor, format) => {
+    const marks = Editor.marks(editor);
+
+    return marks ? marks[format] === true : false;
+  };
+  const toggleMark = (editor, format) => {
+    const isActive = isMarkActive(editor, format);
+
+    if (isActive) {
+      Editor.removeMark(editor, format);
+    } else {
+      Editor.addMark(editor, format, true);
+    }
+  };
+
+  const isBlockActive = (editor, format) => {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === format
+    });
+
+    return !!match;
+  };
+
+  const toggleBlock = (editor, format) => {
+    const isActive = isBlockActive(editor, format);
+    const isList = LIST_TYPES.includes(format);
+
+    Transforms.unwrapNodes(editor, {
+      match: n => LIST_TYPES.includes(n.type),
+      split: true
+    });
+
+    Transforms.setNodes(editor, {
+      type: isActive ? 'paragraph' : isList ? 'list-item' : format
+    });
+
+    if (!isActive && isList) {
+      const block = { type: format, children: [] };
+      Transforms.wrapNodes(editor, block);
+    }
+  };
+
   return (
     <Slate editor={editor} value={value} onChange={value => changevalue(value)}>
+      <div>
+        <button
+          onMouseDown={event => {
+            event.preventDefault();
+            toggleMark(editor, 'bold');
+          }}
+        >
+          Bold
+        </button>
+        <button
+          onMouseDown={event => {
+            event.preventDefault();
+            toggleBlock(editor, 'code');
+          }}
+        >
+          Code
+        </button>
+      </div>
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
@@ -24,28 +132,12 @@ const MarkdownEditor = ({ value, changevalue }) => {
           switch (event.key) {
             case '`': {
               event.preventDefault();
-
-              // Checking to see if the type exists already
-              const [match] = Editor.nodes(editor, {
-                match: n => n.type === 'list-item'
-              });
-
-              // Execute the `insertText` method when the event occurs.
-              Transforms.setNodes(
-                editor,
-                { type: match ? 'paragraph' : 'list-item' },
-                { match: n => Editor.isBlock(editor, n) }
-              );
+              CustomEditor.toggleCodeBlock(editor);
               break;
             }
             case 'b': {
               event.preventDefault();
-
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                { match: n => Text.isText(n), split: true }
-              );
+              toggleMark(editor, 'bold');
               break;
             }
           }
